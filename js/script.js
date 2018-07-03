24101bed3f698effb8e639ed419c8a64a7704ee8
mizunoryoji <40710240+mizunoryoji@users.noreply.github.com>
2018-07-03 19:57:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,95 +22,163 @@ $(function() {
   });
 
 
-  // modal windows
+ // modal windows
 
   // 「.modal-open」をクリック
 
-  $('.modal-open').click(function(){
-
-    // オーバーレイ用の要素を追加
-    $('body').append('<div class="modal-overlay"></div>');
-
-    // オーバーレイをフェードイン
-    $('.modal-overlay').fadeIn('slow');
-
-    // モーダルコンテンツのIDを取得
-    var modal = '#' + $(this).attr('data-target');
-
-    // モーダルコンテンツの表示位置を設定
-
-    modalResize();
-
-    // モーダルコンテンツフェードイン
-
-    $(modal).fadeIn('slow');
+    $('.modal-open').click(function(){
 
 
-    // 「.modal-overlay」あるいは「.modal-close」をクリック
 
-    $('.modal-overlay, .modal-close').off().click(function(){
+         // スクロールバーの横幅を取得
 
-      // モーダルコンテンツとオーバーレイをフェードアウト
-
-      $(modal).fadeOut('slow');
-
-      $('.modal-overlay').fadeOut('slow',function(){
-
-        // オーバーレイを削除
-
-        $('.modal-overlay').remove();
-
-      });
-
-    });
-
-    // リサイズしたら表示位置を再取得
-
-    $(window).on('resize', function(){
-
-      modalResize();
-
-    });
+        var scrollsize = $(window).width() - $('body').prop('clientWidth');
 
 
-    // モーダルコンテンツの表示位置を設定する関数
 
-    function modalResize(){
+        // html、bodyを固定（overflow:hiddenにする）
 
-      // ウィンドウの横幅、高さを取得
-
-      var w = $(window).width();
-
-      var h = $(window).height();
+        $('html, body').addClass('lock');
 
 
-      // モーダルコンテンツの表示位置を取得
 
-      var x = (w - $(modal).outerWidth(true)) / 2;
+        // オーバーレイ用の要素を追加
 
-      var y = (h - $(modal).outerHeight(true)) / 2;
-
-
-      // モーダルコンテンツの表示位置を設定
-
-      $(modal).css({'left': x + 'px','top': y + 'px'});
-
-    }
-  });
+        $('body').append('<div class="modal-overlay"></div>');
 
 
-  $(window).scroll(function (){
-        $('.fadein').each(function(){
-            var elemPos = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 200){
-                $(this).addClass('scrollin');
-            }
+
+        // オーバーレイをフェードイン
+
+        $('.modal-overlay').fadeIn('slow');
+
+
+        // モーダルコンテンツのIDを取得
+
+        var modal = '#' + $(this).attr('data-target');
+
+
+
+         // モーダルコンテンツを囲む要素を追加
+
+        $(modal).wrap("<div class='modal-wrap'></div>");
+
+
+
+        // モーダルコンテンツを囲む要素を表示
+
+        $('.modal-wrap').show();
+
+
+
+        // モーダルコンテンツの表示位置を設定
+
+        modalResize();
+
+
+
+         // モーダルコンテンツフェードイン
+
+        $(modal).fadeIn('slow');
+
+
+
+        // モーダルコンテンツをクリックした時はフェードアウトしない
+
+        $(modal).click(function(e){
+
+            e.stopPropagation();
+
         });
+
+
+        // 「.modal-overlay」あるいは「.modal-close」をクリック
+
+        $('.modal-overlay, .modal-close').off().click(function(){
+
+            // モーダルコンテンツとオーバーレイをフェードアウト
+
+            $(modal).fadeOut('slow');
+
+            $('.modal-overlay').fadeOut('slow',function(){
+
+                // html、bodyの固定解除
+
+                $('html, body').removeClass('lock');
+
+                // オーバーレイを削除
+
+                $('.modal-overlay').remove();
+
+                // モーダルコンテンツを囲む要素を削除
+
+                $(modal).unwrap("<div class='modal-wrap'></div>");
+
+           });
+
+        });
+
+
+
+        // リサイズしたら表示位置を再取得
+
+        $(window).on('resize', function(){
+
+            modalResize();
+
+        });
+
+
+        // モーダルコンテンツの表示位置を設定する関数
+
+        function modalResize(){
+
+            // ウィンドウの横幅、高さを取得
+
+            var w = $(window).width();
+
+            var h = $(window).height();
+
+
+
+            // モーダルコンテンツの横幅、高さを取得
+            var mw = $(modal).outerWidth(true);
+
+            var mh = $(modal).outerHeight(true);
+
+
+
+            // モーダルコンテンツの表示位置を設定
+
+            if ((mh > h) && (mw > w)) {
+
+                $(modal).css({'left': 0 + 'px','top': 0 + 'px'});
+
+            } else if ((mh > h) && (mw < w)) {
+
+                var x = (w - scrollsize - mw) / 2;
+
+                $(modal).css({'left': x + 'px','top': 0 + 'px'});
+
+            } else if ((mh < h) && (mw > w)) {
+
+                var y = (h - scrollsize - mh) / 2;
+
+                $(modal).css({'left': 0 + 'px','top': y + 'px'});
+
+            } else {
+
+                var x = (w - mw) / 2;
+
+                var y = (h - mh) / 2;
+
+                $(modal).css({'left': x + 'px','top': y + 'px'});
+
+            }
+
+        }
+
     });
-
-
 
 });
 
